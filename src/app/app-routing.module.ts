428f1801a55f015e73d16ec3e0c65b12879a672e
ajay kumar <kumar.ajay@adglobal360.com>
2022-07-05 19:34:52 +0530

    added new screem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,6 +29,10 @@ const routes: Routes = [
   {
     path: 'tab4',
     loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },
+  {
+    path: 'member-detail',
+    loadChildren: () => import('./member-detail/member-detail.module').then( m => m.MemberDetailPageModule)
   }
 
 ];
