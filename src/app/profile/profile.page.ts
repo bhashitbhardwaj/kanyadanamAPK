67b8c13b7398271f4112ebe169e12de84a6cb3af
bhashitagl360 <bhashit.bhardwaj@adglobal360.com>
2022-08-02 22:43:01 +0530

    Complete Profile View Page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/provider/api.service';
+import { LoaderService } from 'src/app/provider/loader.service';
+import { ToastService } from 'src/app/provider/toast.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
-  constructor() { }
+  userData: any;
+  constructor(
+    private router: Router,
+    private loader: LoaderService,
+    private api: ApiService,
+    private toast: ToastService
+  ) { }
 
   ngOnInit() {
-  }
+
+    
+    this.api.postDataWithAuth('api/getProfileByUniqueID',  {})
+    .subscribe(res => {
+      if (res.status) {
+        console.log(res.data.user_detail);
+        this.userData = res.data.user_detail;
+      }
+      else {
+        this.toast.Notify({
+          message: res.message,
+          duration: 3000,
+          position: 'top'
+        })
+      }
+    });
+
+    
+}
+  
 
 }
