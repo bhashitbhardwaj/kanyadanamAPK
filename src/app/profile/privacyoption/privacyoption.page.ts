d575eff810ebb7ae2b6a806eab0bc0f424937ea2
bhashitagl360 <bhashit.bhardwaj@adglobal360.com>
2022-08-04 21:17:44 +0530

    privacy,alerts,hideand delete

--- src/app/profile/privacyoption/privacyoption.page.ts
+++ src/app/profile/privacyoption/privacyoption.page.ts
@@ -16,7 +16,7 @@ export class PrivacyoptionPage implements OnInit {
   dropDown: any = {};
   selectedData: any = {};
   public name_privacy = [
-    { val: 'visible', name: '', isChecked: false },
+    { val: 'visible', name: 'Visible to all Members', isChecked: false },
     { val: 'hide', name:'Hide my full name (Displays only Profile ID)', isChecked: false }
   ];
   public phone_privacy = [
@@ -39,11 +39,7 @@ export class PrivacyoptionPage implements OnInit {
     { val: 'yes', name: 'Let other Members know that I have shortlisted their Profile', isChecked: false },
     { val: 'no', name: 'Do not let other Members know that I have shortlisted their Profile', isChecked: false }
   ];
-  public do_not_disturb = [
-    { val: 'instant', isChecked: false },
-    { val: 'daily', isChecked: false },
-    { val: 'unsubscribe', isChecked: false }
-  ];
+  
   public web_notification_privacy = [
     { val: 'subscribe', name:'Subscribe', isChecked: false },
     { val: 'unsubscribe', name: 'Unsubscribe', isChecked: false }
@@ -58,7 +54,94 @@ export class PrivacyoptionPage implements OnInit {
   ) {
   }
 
+  save() {
+    
+    console.log('save:', this.selectedData);
+    this.loader.Show('Loading...');
+    this.api.postDataWithAuth('api/updatePrivacyOptions',
+    {
+      name_privacy:this.selectedData.name_privacy,
+      phone_privacy:this.selectedData.phone_privacy,
+      email_privacy:this.selectedData.email_privacy,
+      income_privacy:this.selectedData.income_privacy,
+      visitor_privacy:this.selectedData.visitor_privacy,
+      shortlist_privacy:this.selectedData.shortlist_privacy,
+      web_notification_privacy:this.selectedData.web_notification_privacy
+    }).subscribe(res=>{
+       this.loader.Hide();
+       if(res.status)
+       {
+         console.log(res);
+         this.toast.Notify({
+          message:res.msg,
+          duration:3000,
+          position:'top'
+        })
+        this.router.navigateByUrl('/profile');
+       }
+       else{
+          this.toast.Notify({
+            message:res.message,
+            duration:3000,
+            position:'top'
+          })
+       }
+    })
+  }
+
   ngOnInit() {
+
+    this.getPrivacyOptions()
+
+  }
+
+  getPrivacyOptions(){
+
+    this.loader.Show('Loading...');
+    this.api.postDataWithAuth('api/getPrivacyOptions',{}).subscribe(res => {
+      this.loader.Hide();
+      if (res.status) {
+        console.log(res.data.user_detail);
+        console.log(Object.keys(res.data.user_detail.privacyOptionArray).length);
+        if(res.data.user_detail.privacyOptionArray && Object.keys(res.data.user_detail.privacyOptionArray).length){
+
+          if(res.data.user_detail.privacyOptionArray.name_privacy!=null){
+            this.selectedData.name_privacy = res.data.user_detail.privacyOptionArray.name_privacy;
+          }
+          if(res.data.user_detail.privacyOptionArray.phone_privacy!=null){
+            this.selectedData.phone_privacy = res.data.user_detail.privacyOptionArray.phone_privacy;
+          }
+          if(res.data.user_detail.privacyOptionArray.email_privacy!=null){
+            this.selectedData.email_privacy = res.data.user_detail.privacyOptionArray.email_privacy;
+          }
+          if(res.data.user_detail.privacyOptionArray.income_privacy!=null){
+            this.selectedData.income_privacy = res.data.user_detail.privacyOptionArray.income_privacy;
+          }
+          if(res.data.user_detail.privacyOptionArray.visitor_privacy!=null){
+            this.selectedData.visitor_privacy = res.data.user_detail.privacyOptionArray.visitor_privacy;
+          }
+          if(res.data.user_detail.privacyOptionArray.shortlist_privacy!=null){
+            this.selectedData.shortlist_privacy = res.data.user_detail.privacyOptionArray.shortlist_privacy;
+          }
+          if(res.data.user_detail.privacyOptionArray.web_notification_privacy!=null){
+            this.selectedData.web_notification_privacy = res.data.user_detail.privacyOptionArray.web_notification_privacy;
+          }
+          
+
+        }
+        
+        
+
+      }
+      else {
+        this.toast.Notify({
+          message: res.message,
+          duration: 3000,
+          position: 'top'
+        })
+      }
+    })
+
   }
 
 }
